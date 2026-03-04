ae4d6ce708c40d095d7b4319e3da7379dfcf282a
winsznx <timjosh507@gmail.com>
2026-03-04 05:27:21 +0100

    Added meta tag to the <head> section of the website homepage to verify domain ownership.

--- packages/webapp/src/app/(marketing)/layout.tsx
+++ packages/webapp/src/app/(marketing)/layout.tsx
@@ -1,5 +1,13 @@
+import type { Metadata } from 'next';
 import { Navbar } from "@/components/Navbar";
 import { marketingNavbarLinks } from "@/lib/navigation";
+
+export const metadata: Metadata = {
+    other: {
+        "talentapp:project_verification":
+            "e4abb6cdf53af84f1133ffbed0496e54c08464d65aa17be5ec28686819eaf28535bdc76c412c4717af27ae3098bcc5a02c2af87124b1f8be387d929bfd17a7e5",
+    },
+};
 
 export default function MarketingLayout({
     children,
