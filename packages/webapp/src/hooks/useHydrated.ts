f016fff2963a334a049a3688a890e32c3bf616e8
winsznx <timjosh507@gmail.com>
2026-03-25 16:58:38 +0100

    docs: describe useHydrated function behavior

--- packages/webapp/src/hooks/useHydrated.ts
+++ packages/webapp/src/hooks/useHydrated.ts
@@ -14,6 +14,9 @@ function getServerSnapshot() {
   return false;
 }
 
+/**
+ * Executes logic associated with use hydrated.
+ */
 export function useHydrated() {
   return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
 }
