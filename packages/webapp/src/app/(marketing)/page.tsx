536369d3b53e46f1bc11964f654bdee43a0e9c32
winsznx <timjosh507@gmail.com>
2026-03-25 16:55:46 +0100

    chore: detail HomePage function behavior

--- packages/webapp/src/app/(marketing)/page.tsx
+++ packages/webapp/src/app/(marketing)/page.tsx
@@ -6,6 +6,9 @@ import { BrandLogo } from '@/components/BrandLogo';
 import { ReceiptStack } from '@/components/ReceiptStack';
 import { footerLegalLinks, footerResourceLinks } from '@/lib/navigation';
 
+/**
+ * Executes logic associated with home page.
+ */
 export default function HomePage() {
   return (
     <div className="min-h-screen">
