57d3fbcc7e1c94dcaf9f8ec1f88c262bc99d0a27
winsznx <timjosh507@gmail.com>
2026-03-25 16:56:19 +0100

    chore: annotate GlobalError function behavior

--- packages/webapp/src/app/global-error.tsx
+++ packages/webapp/src/app/global-error.tsx
@@ -3,6 +3,9 @@
 import { useEffect } from 'react';
 import { AlertTriangle } from 'lucide-react';
 
+/**
+ * Executes logic associated with global error.
+ */
 export default function GlobalError({
   error,
   reset,
