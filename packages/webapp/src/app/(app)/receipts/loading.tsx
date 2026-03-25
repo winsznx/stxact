c102dd41d297a99c4ad75d00da410100744f3b71
winsznx <timjosh507@gmail.com>
2026-03-25 16:55:07 +0100

    style: describe ReceiptsLoading function behavior

--- packages/webapp/src/app/(app)/receipts/loading.tsx
+++ packages/webapp/src/app/(app)/receipts/loading.tsx
@@ -1,5 +1,8 @@
 import { SkeletonTable, SkeletonStat } from '@/components/SkeletonCard';
 
+/**
+ * Executes logic associated with receipts loading.
+ */
 export default function ReceiptsLoading() {
   return (
     <div className="min-h-screen bg-background">
