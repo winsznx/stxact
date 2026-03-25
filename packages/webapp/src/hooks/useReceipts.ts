86dd35b6b417738f847c5a7b866b6ac93236c618
winsznx <timjosh507@gmail.com>
2026-03-25 16:58:45 +0100

    style: annotate useReceipts function behavior

--- packages/webapp/src/hooks/useReceipts.ts
+++ packages/webapp/src/hooks/useReceipts.ts
@@ -1,6 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { api } from '@/lib/api';
 
+/**
+ * Executes logic associated with use receipts.
+ */
 export function useReceipts(params?: {
   seller_principal?: string;
   buyer_principal?: string;
