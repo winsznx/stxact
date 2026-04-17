77f72ebbdb91977bdc56acee73086aea750efc64
winsznx <timjosh507@gmail.com>
2026-04-17 10:44:58 +0100

    chore(webapp): add hooks barrel export

---
+++ packages/webapp/src/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useHydrated } from './useHydrated';
+export { useCurrentEpochSeconds } from './useCurrentEpochSeconds';
+export { useMediaQuery, useIsMobile, useIsTablet, useIsDesktop } from './useMediaQuery';
+export { useDebounce } from './useDebounce';
+export { useClickOutside } from './useClickOutside';
+export { useLocalStorage } from './useLocalStorage';
