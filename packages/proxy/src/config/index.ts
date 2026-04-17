e56c6afde5ce644484f1a5ae98916b6ec86f920b
winsznx <timjosh507@gmail.com>
2026-04-17 09:27:05 +0100

    chore(proxy): add config barrel export

---
+++ packages/proxy/src/config/index.ts
@@ -0,0 +1,5 @@
+export { HTTP_STATUS, type HttpStatusCode } from './http-status';
+export { sendError, badRequest, notFound, conflict, internalError } from './error-response';
+export { ALLOWED_DISPUTE_REASONS, type DisputeReason, isValidDisputeReason } from './dispute-reasons';
+export { SERVICE_CATEGORIES, type ServiceCategory, isValidServiceCategory } from './service-categories';
+export { logger } from './logger';
