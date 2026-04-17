d6208b4114690d1fd8cc170715c3c96c03d704a4
winsznx <timjosh507@gmail.com>
2026-04-17 20:00:46 +0100

    feat(webapp): add usePrevious hook for tracking previous render values

---
+++ packages/webapp/src/hooks/usePrevious.ts
@@ -0,0 +1,9 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T | undefined>(undefined);
+  useEffect(() => { ref.current = value; });
+  return ref.current;
+}
