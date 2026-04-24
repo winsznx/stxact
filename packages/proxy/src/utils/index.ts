366efd911c912a4ba67932742a55ce0516f09885
winsznx <timjosh507@gmail.com>
2026-04-24 16:22:20 +0000

    refactor: generic higher order utility signature preventing input mutation

--- packages/proxy/src/utils/index.ts
+++ packages/proxy/src/utils/index.ts
@@ -1,1 +1,7 @@
 export { isUuid, isStacksPrincipal, isHex64, clampPagination } from './validation';
+
+
+/**
+ * Higher-order generic wrapper ensuring argument immutability in pure functions.
+ */
+export type UtilityFunction<T, R> = (input: Readonly<T>) => R;
